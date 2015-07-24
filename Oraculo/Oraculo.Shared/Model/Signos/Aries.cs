21774a1fb22c3c0b7dd4ed06b62bece4d141302c
andersoncoimbra <andersonmegax@hotmail.com>
2015-07-24 14:30:35 -0300

    Signos
    Criação da primeira classe de Signos que herdam de Signos.cs

---
+++ Oraculo/Oraculo.Shared/Model/Signos/Aries.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oraculo.Model.Signos
+{
+    class Aries : Signo
+    {
+        public override void carregarFrases()
+        {
+            listadefrases.Add("Arrisque, tentativas não sao em vam");
+        }
+    }
+}
